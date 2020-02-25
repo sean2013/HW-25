d10b105eec6f91f839869e3582a1ce42a3d0ba3f
Runali Phillips <rons_millz@yahoo.com>
2020-02-25 11:36:47 -0600

    Using for loop and confirm() method

--- scripts.js
+++ scripts.js
@@ -1,15 +1,32 @@
-// Nested object
-const bio = {
-  name: "Runali",
-  age: 30,
-  parents: {
-    mother: "Rufina",
-    father: "Paulino"
+// Fizz Buzz with for loop
+for(i = 1; i <= 100; i++)
+{
+  if(!(i % 3) && !(i % 5)){
+    console.log("FizzBuzz!");
   }
-};
-console.log(bio.name);
-console.log(bio.parents.father);
+  else if(!(i % 3)){
+    console.log("Fizz!");
+  }
+  else if(!(i % 5)){
+    console.log("Buzz");
+  }
+  else{
+    console.log(i);
+  }
+}
 
-// Re-assign one of the value keys.
-bio.age = 2;
-console.log(bio.age);
+// Create an empty object
+const myObject = {
+}
+// Use confirm() to set a boolean value inside that object
+myObject.message = confirm("Press a button!\nEither OK or Cancel.");
+
+if(myObject.message == true){
+  console.log("You pressed OK!");
+}
+else{
+  console.log("You pressed cancel!");
+}
+
+
+
